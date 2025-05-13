c5988c1c4b91becab856d3f3162ed5b897afd6c1
Preetham Ravichandra <preetham7540@gmail.com>
2025-05-13 18:11:43 +0530

    first commit

---
+++ src/data/projectImages.js
@@ -0,0 +1,9 @@
+import ToDoImage from "../assets/images/ToDo List Image.jpg";
+import ExpenseImage from "../assets/images/Expense Tracker Image.jpg";
+import PortfolioImage from "../assets/images/3D Portfolio Website Image.jpg";
+
+export const projectImages = {
+  todo: ToDoImage,
+  expense: ExpenseImage,
+  portfolio: PortfolioImage,
+};
